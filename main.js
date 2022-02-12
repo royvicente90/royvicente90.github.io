46d3b288098b7f6b0972182dc94cb652076f0af1
royvicente90 <r.roy@correo.unimete.edu.ve>
2022-02-12 12:04:34 -0400

    hero falta fix

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 //window.setTimeout(function () {
 //    window.location.reload();
-//  }, 3000);
+//  }, 10000);
+
+
+window.addEventListener('load', function () {
+        console.log('el contenido se ha cargado');
+        var imagenes = [];
+        imagenes[0] = 'img/Foto 1.png';
+        imagenes[1] = 'img/Foto 2.png';
+        imagenes[2] = 'img/Foto 3.png';
+        
+        var IndiceImagenes = 0;
+        function cambiarImagenes(){
+            document.slider.src = imagenes[IndiceImagenes];    
+            if(IndiceImagenes < 2) {
+                IndiceImagenes++
+            } else {
+                IndiceImagenes = 0;
+            }
+        }
+        setInterval(cambiarImagenes, 10000);
+    })
